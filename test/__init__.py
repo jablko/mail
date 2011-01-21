c59ab9d5452489814f6c661460db912d193f92ec
Jack Bates <ms419@freezone.co.uk>
2011-01-21 08:55:40 -0800

    Add expect() and count assertions. Consider using one of the other styles from, http://www.python.org/dev/peps/pep-3130/

--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,21 @@
+# Workaround, http://www.python.org/dev/peps/pep-3130/
+config = { 'count': 0 }
+
 def ok(condition, actual):
-  print 'PASS' if condition else 'FAIL "%s"' actual
+  config['count'] += 1
+
+  print 'PASS' if condition else 'FAIL "%s"' % actual
 
   return condition
 
-def equal(actual, expected):
-  condition = expected == actual
+def equal(actual, expect):
+  config['count'] += 1
 
-  print 'PASS' if condition else 'FAIL "%s" "%s"' % (actual, expected)
+  condition = expect == actual
+
+  print 'PASS' if condition else 'FAIL "%s" "%s"' % (actual, expect)
 
   return condition
+
+def expect(count):
+  config['expect'] = count
