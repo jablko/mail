9067ff42d446b8945e385eaefd06b94eeac97dc0
Jack Bates <jack@nottheoilrig.com>
2013-03-17 15:04:56 -0700

    Cosmetic

--- test/__init__.py
+++ test/__init__.py
@@ -4,11 +4,11 @@ from twisted.internet import reactor
 from twisted.python import log
 from untwisted import promise
 
-def sdfg(cbl):
+def sdfg(callback):
   log.startLogging(sys.stdout)
 
   # Test complete whether success or exception
-  cbl().then(promise.promise()).then(lambda _: reactor.stop())
+  callback().then(promise.promise()).then(lambda _: reactor.stop())
 
   reactor.callLater(3, reactor.stop)
   reactor.run()
